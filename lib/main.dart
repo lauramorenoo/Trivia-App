e9130560319c8091c5d6fca0650a560675ea83f8
Laura Moreno <lmoreno29@csub.edu>
2021-12-03 16:47:00 -0800

    changed dependencies

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,11 @@
 import 'package:flutter/material.dart';
 import './quiz.dart';
+import 'package:game_socket/game_socket.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
-      home: TriviaQuiz(),
-    )
-  );
+  runApp(MaterialApp(
+    home: TriviaQuiz(),
+  ));
 }
 
 class TriviaQuiz extends StatefulWidget {
@@ -24,34 +23,29 @@ class TriviaQuizState extends State<TriviaQuiz> {
         title: const Text("Cloud Race Trivia"),
         backgroundColor: Colors.blueGrey,
       ),
-
       body: Container(
         margin: const EdgeInsets.all(15.0),
         child: Column(
-          
           crossAxisAlignment: CrossAxisAlignment.stretch,
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            
             MaterialButton(
                 height: 50.0,
                 color: Colors.green,
                 onPressed: startQuiz,
-                child: Text("Start Quiz",
-                style: new TextStyle(
-                  fontSize: 18.0,
-                  color: Colors.white
-                ),)
-            )
+                child: Text(
+                  "Start Quiz",
+                  style: new TextStyle(fontSize: 18.0, color: Colors.white),
+                ))
           ],
         ),
       ),
     );
   }
 
-  void startQuiz(){
-    setState((){
-      Navigator.push(context, MaterialPageRoute(builder: (context)=> Quiz()));
-  });
+  void startQuiz() {
+    setState(() {
+      Navigator.push(context, MaterialPageRoute(builder: (context) => Quiz()));
+    });
   }
 }
